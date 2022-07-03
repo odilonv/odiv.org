8dfa39c89a0687aa099956c312e280c073b9e453
Odilon <odilon.vidal@icloud.com>
2022-07-03 13:45:00 +0200

    last

--- app.js
+++ app.js
@@ -74,21 +74,63 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     const element = document.querySelector(".reseaux");
     const element2 = document.querySelector(".arcdecercleL");
-    const element3 = document.querySelector(".rectanglehorizontal");
+    const element3 = document.querySelector(".rectangleright");
+    const element4 = document.querySelector(".rectanglehorizontal");
+    const element5 = document.querySelector(".arcdecercleR");
+    const element6 = document.querySelector(".arcdecercleR2");
+    const element7 = document.querySelector(".rectangleleft");
+
     if (document.body.scrollTop > 655 || document.documentElement.scrollTop > 655)
     {
-        element.style.position= "absolute" ;
-        element.style.bottom= "-550px";
-        element2.style.position= "absolute" ;
-        element2.style.bottom= "-375px";
-    }
+        if(document.body.scrollTop < 1050 && document.documentElement.scrollTop < 1050){
+            element.style.position= "absolute" ;
+            element.style.bottom= "-550px";
+            element2.style.position= "absolute" ;
+            element2.style.bottom= "-375px";
+            element3.style.position="fixed";
+            element3.style.bottom="0px";
+            element4.style.right="-1000px";
+            element5.style.position="fixed";
+            element5.style.bottom="100px";
+            element4.style.opacity="0";
+            element6.style.opacity="0";
+            element7.style.background="white";
+            element7.style.width="1px";
+            element7.style.height="100px";
 
+
+        }
+        else {
+            element3.style.position="absolute";
+            element3.style.bottom="-1050px";
+            element4.style.right="55px";
+            element5.style.position="absolute"
+            element5.style.bottom="-950px";
+            element4.style.opacity="1";
+            element6.style.opacity="1";
+            element7.style.opacity="0.5";
+            element7.style.background ="linear-gradient(-90deg,#590000 35%, #C7353D)";
+            element7.style.width="100%";
+            element7.style.height="10px";
+        }
+    }
     else
     {
         element.style.position= "fixed";
         element.style.bottom= "105px";
         element2.style.position= "fixed";
         element2.style.bottom= "280px";
+        element3.style.position="fixed";
+        element3.style.bottom="0px";
+        element4.style.right="-1000px";
+        element5.style.position="fixed";
+        element5.style.bottom="100px";
+        element4.style.opacity="0";
+        element6.style.opacity="0";
+        element7.style.background="white";
+        element7.style.width="1px";
+        element7.style.height="100px";
+
 
 
     }
