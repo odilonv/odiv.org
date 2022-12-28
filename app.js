ef65c48286c32c777d1310e393c38d4e21cc7cd2
Odilon VIDAL <odilon.vidal@etu.umontpellier.fr>
2022-12-28 01:48:49 +0100

    last

--- app.js
+++ app.js
@@ -1,29 +1,3 @@
-let section = document.querySelectorAll("section");
-let menu = document.querySelectorAll(".links-circle");
-
-const btnCircle = document.querySelector('.btn-circle');
-const circleMenu = document.querySelector('.circle-menu');
-const contact = document.querySelector('.contact-circle');
-const contactc = document.querySelector('.contact-corner');
-let i = 0;
-
-btnCircle.addEventListener('click', () => {
-    i++;
-    if(i & 1){
-        contact.style.opacity = 0;
-        contactc.style.top = '-30px';
-    }
-    else {
-        contact.style.opacity = 1;
-        contactc.style.top = '65px';
-        contactc.style.right = '150px';
-    }
-    btnCircle.classList.toggle('menu-anim');
-    circleMenu.classList.toggle('circle-anim');
-
-})
-
-
 var text = document.getElementById('text');
 var parallaxInstance = new Parallax(text);
 var scene = document.getElementById('scene');
@@ -31,10 +5,47 @@ var parallaxInstance = new Parallax(scene);
 
 
 
+
+let section = document.querySelectorAll("section");
+let menu = document.querySelectorAll(".links-circle");
+
+
+
+
+const btnCircle = document.querySelector('.btn-circle');
+const circleMenu = document.querySelector('.circle-menu');
+const contact = document.querySelector('.contact-circle');
+
+let i = 0;
+
+btnCircle.addEventListener('click', () => {
+    i++;
+    if(i & 1){
+        contact.style.right = '20px';
+        contact.style.opacity = 0;
+
+    }
+    else {
+
+
+        contact.style.opacity = 1;
+        contact.style.right = '100px';
+    }
+    btnCircle.classList.toggle('menu-anim');
+    circleMenu.classList.toggle('circle-anim');
+
+}
+)
+
+
+
+
+
+
 window.onscroll = () => {
     section.forEach((i) => {
         let top = window.scrollY;
-        let offset = i.offsetTop - 150;
+        let offset = i.offsetTop-10;
         let height = i.offsetHeight;
         let id = i.getAttribute("id");
 
@@ -70,70 +81,70 @@ window.addEventListener("scroll", reveal);
 reveal();
 
 
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-    const element = document.querySelector(".reseaux");
-    const element2 = document.querySelector(".arcdecercleL");
-    const element3 = document.querySelector(".rectangleright");
-    const element4 = document.querySelector(".rectanglehorizontal");
-    const element5 = document.querySelector(".arcdecercleR");
-    const element6 = document.querySelector(".arcdecercleR2");
-    const element7 = document.querySelector(".rectangleleft");
-
-    if (document.body.scrollTop > 655 || document.documentElement.scrollTop > 655)
-    {
-        if(document.body.scrollTop < 1050 && document.documentElement.scrollTop < 1050){
-            element.style.position= "absolute" ;
-            element.style.bottom= "-550px";
-            element2.style.position= "absolute" ;
-            element2.style.bottom= "-375px";
-            element3.style.position="fixed";
-            element3.style.bottom="0px";
-            element5.style.position="fixed";
-            element5.style.bottom="100px";
-            element6.style.opacity="0";
-            element7.style.opacity="0";
-            //element7.style.background="white";
-            //element7.style.width="1px";
-            //element7.style.height="100px";
-            //element4.style.right="-1000px";
-            //element4.style.opacity="0";
-
-
-        }
-        else {
-            element3.style.position="absolute";
-            element3.style.bottom="-1050px";
-            element5.style.position="absolute"
-            element5.style.bottom="-950px";
-            element6.style.opacity="1";
-            /*element7.style.opacity="0.5";
-            element7.style.background ="linear-gradient(-90deg,#590000 35%, #C7353D)";
-            element7.style.width="100%";
-            element7.style.height="10px";*/
-            //element4.style.right="55px";
-            //element4.style.opacity="1";
-        }
-    }
-    else
-    {
-        element.style.position= "fixed";
-        element.style.bottom= "105px";
-        element2.style.position= "fixed";
-        element2.style.bottom= "280px";
-        element3.style.position="fixed";
-        element3.style.bottom="0px";
-        element5.style.position="fixed";
-        element5.style.bottom="100px";
-        element6.style.opacity="0";
-        element7.style.opacity="1";
-        /*element7.style.background="white";
-        element7.style.width="1px";
-        element7.style.height="100px";*/
-        //element4.style.right="-1000px";
-        //element4.style.opacity="0";
-
-
-
-    }
-}
+// window.onscroll = function() {scrollFunction()};
+// function scrollFunction() {
+//     const element = document.querySelector(".reseaux");
+//     const element2 = document.querySelector(".arcdecercleL");
+//     const element3 = document.querySelector(".rectangleright");
+//     const element4 = document.querySelector(".rectanglehorizontal");
+//     const element5 = document.querySelector(".arcdecercleR");
+//     const element6 = document.querySelector(".arcdecercleR2");
+//     const element7 = document.querySelector(".rectangleleft");
+//
+//     if (document.body.scrollTop > 655 || document.documentElement.scrollTop > 655)
+//     {
+//         if(document.body.scrollTop < 1050 && document.documentElement.scrollTop < 1050){
+//             element.style.position= "absolute" ;
+//             element.style.bottom= "-550px";
+//             element2.style.position= "absolute" ;
+//             element2.style.bottom= "-375px";
+//             element3.style.position="fixed";
+//             element3.style.bottom="0px";
+//             element5.style.position="fixed";
+//             element5.style.bottom="100px";
+//             element6.style.opacity="0";
+//             element7.style.opacity="0";
+//             //element7.style.background="white";
+//             //element7.style.width="1px";
+//             //element7.style.height="100px";
+//             //element4.style.right="-1000px";
+//             //element4.style.opacity="0";
+//
+//
+//         }
+//         else {
+//             element3.style.position="absolute";
+//             element3.style.bottom="-1050px";
+//             element5.style.position="absolute"
+//             element5.style.bottom="-950px";
+//             element6.style.opacity="1";
+//             /*element7.style.opacity="0.5";
+//             element7.style.background ="linear-gradient(-90deg,#590000 35%, #C7353D)";
+//             element7.style.width="100%";
+//             element7.style.height="10px";*/
+//             //element4.style.right="55px";
+//             //element4.style.opacity="1";
+//         }
+//     }
+//     else
+//     {
+//         element.style.position= "fixed";
+//         element.style.bottom= "105px";
+//         element2.style.position= "fixed";
+//         element2.style.bottom= "280px";
+//         element3.style.position="fixed";
+//         element3.style.bottom="0px";
+//         element5.style.position="fixed";
+//         element5.style.bottom="100px";
+//         element6.style.opacity="0";
+//         element7.style.opacity="1";
+//         /*element7.style.background="white";
+//         element7.style.width="1px";
+//         element7.style.height="100px";*/
+//         //element4.style.right="-1000px";
+//         //element4.style.opacity="0";
+//
+//
+//
+//     }
+// }
