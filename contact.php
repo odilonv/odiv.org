e812201c8199de61ce722387b32e4fbd60c53d9b
Odilon <odilon.vidal@icloud.com>
2023-01-31 02:40:35 +0100

    update v1.2

--- contact.php
+++ contact.php
@@ -4,13 +4,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>ODIV</title>
-    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico">
+    <title>ODIV - Contact</title>
+    <link rel="manifest" href="/manifest.json">
     <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico">
+    <link rel="preload" as="font"  href="assets/fonts/Montserrat-Regular.ttf" crossorigin>
+    <link rel="preload" as="font" href="assets/fonts/Akira%20Expanded%20Demo.otf" crossorigin>
 
-    <script src="https://kit.fontawesome.com/e789a2f629.js" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js"></script>
+
 </head>
 
 <body>
@@ -38,11 +39,11 @@
 </header>
 
 <ul class="reseaux" id="reseaux-contact">
-    <li><a href="https://www.linkedin.com/in/odilon-vidal/"><i class="fab fa-linkedin"></i></a></li>
-    <li><a href="https://github.com/odilonv"><i class="fab fa-github"></i></a></li>
-    <li><a href="https://www.behance.net/odiv"><i class="fab fa-behance"></i></a></li>
-    <li><a href="https://www.twitter.com/odiv_v"><i class="fab fa-twitter"></i></a></li>
-    <li><a href="https://www.tiktok.com/odiv_v"><i class="fab fa-tiktok"></i></a></li>
+    <li><a aria-label="linkedin" href="https://www.linkedin.com/in/odilon-vidal/"><i class="fab fa-linkedin"></i></a></li>
+    <li><a aria-label="github" href="https://github.com/odilonv"><i class="fab fa-github"></i></a></li>
+    <li><a aria-label="behance" href="https://www.behance.net/odiv"><i class="fab fa-behance"></i></a></li>
+    <li><a aria-label="twitter" href="https://www.twitter.com/odiv_v"><i class="fab fa-twitter"></i></a></li>
+    <li><a aria-label="tiktok" href="https://www.tiktok.com/odiv_v"><i class="fab fa-tiktok"></i></a></li>
 </ul>
 
 <div class="rectangleleft"></div>
@@ -112,6 +113,7 @@
 </div>
 
 <script src="assets/js/app.js"></script>
+<script src="https://kit.fontawesome.com/e789a2f629.js" crossorigin="anonymous"></script>
 
 </body>
 </html>
