74d5befda03344183554c73d69d7a9b2dcde5fcb
Odilon <odilon.vidal@etu.umontpellier.fr>
2023-03-29 18:06:20 +0200

    update

--- assets/js/projets/richvote.js
+++
@@ -1,5 +0,0 @@
-// changer la couleur de fond de la page
-document.body.style.transition='5s';
-document.body.style.background = "radial-gradient(circle, rgba(199,53,61,1) 0%, rgba(243,92,92,1) 32%, rgba(199,72,72,1) 100%)";
-document.body.style.background = "rgba(157, 158, 252,1)";
-
